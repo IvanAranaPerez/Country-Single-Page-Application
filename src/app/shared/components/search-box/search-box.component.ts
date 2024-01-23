40f1855deaf199d80ebca0a8932e16f19ae7d087
IvanAranaPerez <i.arana@dappertechnologies.com>
2024-01-23 13:24:19 -0500

    refactorizacion del servicio e implementacion del loader con debouncer

--- src/app/shared/components/search-box/search-box.component.ts
+++ src/app/shared/components/search-box/search-box.component.ts
@@ -1,19 +1,35 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'shared-search-box',
   templateUrl: './search-box.component.html',
 })
-export class SearchBoxComponent {
+export class SearchBoxComponent implements OnInit {
+
+  private debouncer: Subject<string> = new Subject<string>();
 
   @Input()
   public placeholder: string = '';
 
   @Output()
-  public onValue = new EventEmitter<string>();
+  public onValue = new EventEmitter<string>(); //cuando se presiona Enter
+
+  @Output()
+  public onDebounce = new EventEmitter<string>(); //cuando se deja de escribir en el teclado
+
+  ngOnInit(): void {
+      this.debouncer.pipe( debounceTime(500) ).subscribe( value => {
+          this.onDebounce.emit(value);
+      });
+  }
 
   emitValue(value: string):void{
     this.onValue.emit(value);
+  }
+
+  onKeyPress(searchTerm: string){
+    this.debouncer.next(searchTerm);
   }
 
 
